86a55751d13bac4f9ab5487b9b4022dbfbe47846
abhisheksharsoor <abhisheksharsoor@gmail.com>
2022-05-02 12:14:39 +0530

    addingScholarship

--- Admin_Dashboard_Panel/createscholarship.php
+++ Admin_Dashboard_Panel/createscholarship.php
@@ -11,6 +11,289 @@ include("admin-layout.php")
     
 <!-- Add contents here -->
 
+<style>
+  .card {
+    border-radius: 5px;
+    background: rgba(236, 240, 243, 0.6);
+    box-shadow: 13px 13px 20px #cbced1,
+        -13px -13px 20px #ffffff;
+    height: 400px;
+}
+</style>
+
+<section>
+<div class="container p-4">
+  <h2>Scholarship</h2>
+<!-- Cards Start -->
+<div class="row">
+<div class="col-sm-4">
+  <div class="card mt-5 text-center">
+  <div class="card-header">
+     <i class="fa-solid fa-calendar-days px-2"></i>Deadline: 26/04/2022
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Scholarship Name</h5>
+    <p class="card-text">Eligibility</p>
+    <form method="POST" action="add-scholarship.php">
+      <input type="submit"class="btn btn-primary"  value="Add Details"></input>
+    </form>
+   
+  </div>
+  <div class="card-footer">
+    <a href="#" class="text-decoration-none text-dark">View Scholarship</a>
+  </div>
+</div>
+</div>
+
+<div class="col-sm-4">
+<div class="card mt-5 text-center">
+  <div class="card-header">
+     <i class="fa-solid fa-calendar-days px-2"></i>Deadline: 26/04/2022
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Scholarship Name</h5>
+    <p class="card-text">Eligibility</p>
+    <form method="POST" action="add-scholarship.php">
+      <input type="submit"class="btn btn-primary"  value="Add Details"></input>
+    </form>
+   
+  </div>
+  <div class="card-footer text-muted">
+    View Scholarship
+  </div>
+</div>
+</div>
+
+<div class="col-sm-4">
+<div class="card mt-5 text-center">
+  <div class="card-header">
+     <i class="fa-solid fa-calendar-days px-2"></i>Deadline: 26/04/2022
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Scholarship Name</h5>
+    <p class="card-text">Eligibility</p>
+    <form method="POST" action="add-scholarship.php">
+      <input type="submit"class="btn btn-primary"  value="Add Details"></input>
+    </form>
+   
+  </div>
+  <div class="card-footer text-muted">
+    View Scholarship
+  </div>
+</div>
+</div>
+</div>
+<!-- Cards End -->
+</div>
+
+
+  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+<div class="container">
+<div class="btn-group" role="group" aria-label="Basic outlined example">
+    <button type="button" class="btn btn-outline-primary" onclick="window.location.href = '#';">SC/CT/OBC</button>
+    <button type="button" class="btn btn-outline-primary" onclick="window.location.href = '#';">GIRLS</button>
+    <button type="button" class="btn btn-outline-primary" onclick="window.location.href = '#';">MINORITY</button>
+    <button type="button" class="btn btn-outline-primary" onclick="window.location.href = '#';">PHYSICALLY DISABLED</button>
+</div>
+</div>
+
+<!-- Cards Start -->
+<div class="row">
+<div class="col-sm-4">
+<div class="card mt-5 text-center">
+  <div class="card-header">
+     <i class="fa-solid fa-calendar-days px-2"></i>Deadline: 26/04/2022
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Scholarship Name</h5>
+    <p class="card-text">Eligibility</p>
+    <form method="POST" action="add-scholarship.php">
+      <input type="submit"class="btn btn-primary"  value="Add Details"></input>
+    </form>
+   
+  </div>
+  <div class="card-footer text-muted">
+    View Scholarship
+  </div>
+</div>
+</div>
+
+<div class="col-sm-4">
+<div class="card mt-5 text-center">
+  <div class="card-header">
+     <i class="fa-solid fa-calendar-days px-2"></i>Deadline: 26/04/2022
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Scholarship Name</h5>
+    <p class="card-text">Eligibility</p>
+    <form method="POST" action="add-scholarship.php">
+      <input type="submit"class="btn btn-primary"  value="Add Details"></input>
+    </form>
+   
+  </div>
+  <div class="card-footer text-muted">
+    View Scholarship
+  </div>
+</div>
+</div>
+
+<div class="col-sm-4">
+<div class="card mt-5 text-center">
+  <div class="card-header">
+     <i class="fa-solid fa-calendar-days px-2"></i>Deadline: 26/04/2022
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Scholarship Name</h5>
+    <p class="card-text">Eligibility</p>
+    <form method="POST" action="add-scholarship.php">
+      <input type="submit"class="btn btn-primary"  value="Add Details"></input>
+    </form>
+   
+  </div>
+  <div class="card-footer text-muted">
+    View Scholarship
+  </div>
+</div>
+</div>
+</div>
+<!-- Cards End -->
+  </div>
+  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+  <div class="container">
+  <div class="btn-group" role="group" aria-label="Basic outlined example">
+    <button type="button" class="btn btn-outline-primary" onclick="window.location.href = '#';">NATIONAL</button>
+    <button type="button" class="btn btn-outline-primary" onclick="window.location.href = '#';">KARNATAKA</button>
+</div>
+    </div>
+
+    <!-- Cards Start -->
+<div class="row">
+<div class="col-sm-4">
+<div class="card mt-5 text-center">
+  <div class="card-header">
+     <i class="fa-solid fa-calendar-days px-2"></i>Deadline: 26/04/2022
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Scholarship Name</h5>
+    <p class="card-text">Eligibility</p>
+    <form method="POST" action="add-scholarship.php">
+      <input type="submit"class="btn btn-primary"  value="Add Details"></input>
+    </form>
+   
+  </div>
+  <div class="card-footer text-muted">
+    View Scholarship
+  </div>
+</div>
+</div>
+
+<div class="col-sm-4">
+<div class="card mt-5 text-center">
+  <div class="card-header">
+     <i class="fa-solid fa-calendar-days px-2"></i>Deadline: 26/04/2022
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Scholarship Name</h5>
+    <p class="card-text">Eligibility</p>
+    <form method="POST" action="add-scholarship.php">
+      <input type="submit"class="btn btn-primary"  value="Add Details"></input>
+    </form>
+   
+  </div>
+  <div class="card-footer text-muted">
+    View Scholarship
+  </div>
+</div>
+</div>
+
+<div class="col-sm-4">
+<div class="card mt-5 text-center">
+  <div class="card-header">
+     <i class="fa-solid fa-calendar-days px-2"></i>Deadline: 26/04/2022
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Scholarship Name</h5>
+    <p class="card-text">Eligibility</p>
+    <form method="POST" action="add-scholarship.php">
+      <input type="submit"class="btn btn-primary"  value="Add Details"></input>
+    </form>
+   
+  </div>
+  <div class="card-footer text-muted">
+    View Scholarship
+  </div>
+</div>
+</div>
+</div>
+<!-- Cards End -->
+</div>
+  <div class="tab-pane fade" id="nav-non-govt" role="tabpanel" aria-labelledby="nav-non-govt-tab">
+    <!-- Cards Start -->
+<div class="row">
+  <div class="col-sm-4">
+<div class="card mt-5 text-center">
+  <div class="card-header">
+    <i class="fa-solid fa-calendar-days px-2"></i>Deadline: 26/04/2022
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Scholarship Name</h5>
+    <p class="card-text">Eligibility</p>
+    <form method="POST" action="add-scholarship.php">
+      <input type="submit"class="btn btn-primary"  value="Add Details"></input>
+    </form>
+   
+  </div>
+  <div class="card-footer text-muted">
+    View Scholarship
+  </div>
+</div>
+</div>
+
+  <div class="col-sm-4">
+<div class="card mt-5 text-center">
+  <div class="card-header">
+    <i class="fa-solid fa-calendar-days px-2"></i>Deadline: 26/04/2022
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Scholarship Name</h5>
+    <p class="card-text">Eligibility</p>
+    <form method="POST" action="add-scholarship.php">
+      <input type="submit"class="btn btn-primary"  value="Add Details"></input>
+    </form>
+   
+  </div>
+  <div class="card-footer text-muted">
+    View Scholarship
+  </div>
+</div>
+</div>
+
+  <div class="col-sm-4">
+<div class="card mt-5 text-center">
+  <div class="card-header">
+    <i class="fa-solid fa-calendar-days px-2"></i>Deadline: 26/04/2022
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Scholarship Name</h5>
+    <p class="card-text">Eligibility</p>
+    <form method="POST" action="add-scholarship.php">
+      <input type="submit"class="btn btn-primary"  value="Add Details"></input>
+    </form>
+   
+  </div>
+  <div class="card-footer text-muted">
+    View Scholarship
+  </div>
+</div>
+
+<!-- Cards End -->
+  </div>
+</div>
+<div>
+</section>
+
+
+
 <!-- End of content  -->
 
 
