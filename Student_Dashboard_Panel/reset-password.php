000fc5b2408b5fcd9b42cdbd11c944277bbf09dd
Gulam Rabbani <g.rabbani4me07@gmail.com>
2022-05-06 20:18:41 +0530

    update

--- Student_Dashboard_Panel/reset-password.php
+++ Student_Dashboard_Panel/reset-password.php
@@ -86,7 +86,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
-                <a class="btn btn-danger" href="scholarship.php">Cancel</a>
+                <a class="btn btn-danger" href="live-scholarship.php">Cancel</a>
             </div>
         </form>
       </div>
