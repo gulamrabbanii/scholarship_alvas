6b8c1a5a0cba7a5126d88b9a342893e86b991dee
Gulam Rabbani <g.rabbani4me07@gmail.com>
2022-05-18 22:28:27 +0530

    update

--- Admin_Dashboard_Panel/inactive-scholarship.php
+++ Admin_Dashboard_Panel/inactive-scholarship.php
@@ -5,166 +5,181 @@ require_once("../db/config.php");
 $sql = "SELECT * FROM scholarship_details t1 INNER JOIN elig_req t2 ON t2.sch_name = t1.sch_name WHERE (status != 'active') ORDER BY created_at DESC";
 ?>
 <title>INACTIVE SCHOLARSHIPS</title>
-        <div class="dash-content">
-            <div class="overview">
-                <div class="title">
-                    <i class="uil uil-tachometer-fast-alt"></i>
-                    <span class="text">
+<div class="dash-content">
+    <div class="overview">
+        <div class="title">
+            <i class="uil uil-tachometer-fast-alt"></i>
+            <span class="text">
 
-<div class="container">
-  <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-  <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off">
-  <label class="btn btn-outline-primary" for="btnradio1" onclick="window.location.href = 'all-scholarships.php';">ALL SCHOLARSHIP</label>
+                <div class="container">
+                    <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                        <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="btnradio1" onclick="window.location.href = 'all-scholarships.php';">ALL SCHOLARSHIP</label>
 
-  <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-  <label class="btn btn-outline-primary" for="btnradio2" onclick="window.location.href = 'live-scholarship.php';">LIVE SCHOLARSHIP</label>
+                        <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="btnradio2" onclick="window.location.href = 'live-scholarship.php';">LIVE SCHOLARSHIP</label>
 
-  <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-  <label class="btn btn-outline-primary" for="btnradio3" onclick="window.location.href = 'upcoming-scholarship.php';">UPCOMING SCHOLARSHIP</label>
+                        <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="btnradio3" onclick="window.location.href = 'upcoming-scholarship.php';">UPCOMING SCHOLARSHIP</label>
 
-  <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off" checked>
-  <label class="btn btn-outline-primary" for="btnradio3" onclick="window.location.href = 'inactive-scholarship.php';">INACTIVE SCHOLARSHIP</label>
-</div>
-</div>
-
-
-                    </span>
+                        <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off" checked>
+                        <label class="btn btn-outline-primary" for="btnradio3" onclick="window.location.href = 'inactive-scholarship.php';">INACTIVE SCHOLARSHIP</label>
+                    </div>
                 </div>
-    
-<!-- Add contents here -->
-<style>
-  .card {
-    border-radius: 5px;
-    background: rgba(236, 240, 243, 0.6);
-    height: 450px;
-}
-.card-text-body {
-  margin-top: 10px;
-  height: calc(100% - 300px);
-}
-.txt:hover {
-    text-decoration: underline;
-}
-p {
-margin: 0;
-padding: 2px;
-text-align: justify;
-text-justify:initial;
-word-break: break-all;
-line-height: 1;
-}
-</style>
 
 
-<!-- Cards Start -->
-<div class="row">
-<?php
-if($result = $link->query($sql)){
-    if($result->num_rows > 0){
-        while($row = $result->fetch_array()){ ?>
-                <div class="col-sm-4">
-                <div class="card mt-3">
-                <div class="card-header text-center <?php echo ($row['sch_start_date'] > date("Y-m-d")) ? 'bg-success' : 'bg-danger'; ?>">
-                <i class="fa-solid fa-calendar-days px-2"></i><?php echo ($row['sch_start_date'] > date("Y-m-d")) ? "Launch Date: " . $row["sch_start_date"] : "Deadline: " . $row["sch_deadline"]; ?>
-                </div>
-                <div class="card-body">
-                <h5 class="card-title txt"><a class="text-decoration-none text-secondary" href="<?php echo $row['sch_link'] ?>" target="_blank" rel="noopener noreferrer"><?php echo $row["sch_name"] ?></a></h5>
-                <hr>
-                <div class="card-text-body">
-                      <div class="card-text text-primary">Eligibility</div>
-                        <ul>
-                        <?php if(!empty($row['minority'])) {?>
-                        <li><p><small class="text-muted"><?php echo $row['minority']; ?></small></p></li>
-                        <?php }?>
-                        <?php if(!empty($row['sc_st'])) {?>
-                        <li><p><small class="text-muted"><?php echo $row['sc_st']; ?></small></p></li>
-                        <?php }?>
-                        <?php if(!empty($row['girls'])) {?>
-                        <li><p><small class="text-muted"><?php echo $row['girls']; ?></small></p></li>
-                        <?php }?><?php if(!empty($row['community'])) {?>
-                        <li><p><small class="text-muted"><?php echo $row['community']; ?></small></p></li>
-                        <?php }?>
-                        <?php if(!empty($row['pwd'])) {?>
-                        <li><p><small class="text-muted"><?php echo $row['pwd']; ?></small></p>
-                        <?php }?></li>
-                        <?php if(!empty($row['athletic'])) {?>
-                        <li><p><small class="text-muted"><?php echo $row['athletic']; ?></small></p></li>
-                        <?php }?>
-                        <?php if(!empty($row['other_sch'])) {?>
-                        <li><p><small class="text-muted"><?php echo $row['other_sch']; ?></small></p></li>
-                       <?php }?>
-                    <ul>                     
-                </div>         
-                </div>
-                <div class="card-footer d-flex justify-content-between">
-                <small> Last Updated On:</small>
-                <small class="text-muted"><?php echo $row['created_at'] ?></small>   
-                </div>
-                <div class="card-footer">
-                  <div class="d-flex justify-content-center">
-                      <div class="btn btn-sm"><i class="fas fa-eye" aria-hidden="true">
-                        <?php echo "<a class='px-1 text-decoration-none text-muted' onclick=\"return confirm('Do you really want to Enable this scholarship?')\" href=\"../scholarship-operation/activate-scholarship.php?id=" . $row['id'] . " \">ENABLE</a>"; ?>
-                            <script>
-                                document.getElementById('a.delete').on('click', function() {
-                                    var choice = confirm('Enable this scholarship?');
-                                    if (choice === true) {
-                                        return true;
-                                    }
-                                    return false;
-                                });
-                            </script>
-                      </i></div>
-                      <div class="btn btn-sm"><i class="fas fa-edit" aria-hidden="true">
-                          <?php echo "<a class='px-1 text-decoration-none text-success' onclick=\"return confirm('Do you really want to modify this scholarship?')\" href=\"update-scholarship.php?id=" . $row['id'] . " \">MODIFY</a>"; ?>
-                            <script>
-                                document.getElementById('a.delete').on('click', function() {
-                                    var choice = confirm('Modify this scholarship?');
-                                    if (choice === true) {
-                                        return true;
-                                    }
-                                    return false;
-                                });
-                            </script>
-                      </i></div>
-                      <div class="btn btn-sm"><i class="fa fa-trash" aria-hidden="true">
-                          <?php echo "<a class='px-1 text-decoration-none text-danger' onclick=\"return confirm('Do you really want to delete this scholarship?')\" href=\"../scholarship-operation/delete-scholarship.php?id=" . $row['id'] . " \">DELETE</a>"; ?>
-                            <script>
-                                document.getElementById('a.delete').on('click', function() {
-                                    var choice = confirm('Delete this scholarship?');
-                                    if (choice === true) {
-                                        return true;
-                                    }
-                                    return false;
-                                });
-                            </script>
-                      </i></div>
-                      </div>
-                       </div>
-                 <div class="card-footer text-center bg-primary">
-                <a href="../scholarship-operation/scholarship-details.php?id=<?php echo urlencode($row['id']) ?>" class="text-decoration-none text-white">View Scholarship</a>
-                </div>
-            </div>
-            </div>            
-<?php    } 
-        // Free result set
-        $result->free();
-    } else{
-        echo "<br>";
-        echo "There are currently no scholarships inactive.";
-    }
-} else{
-    echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
-}
-?>
-</div>
-<!-- Cards End -->
-
-
-
-<!-- End of content  -->
-
-
-            </div>
+            </span>
         </div>
-    </section>
-        
+
+        <!-- Add contents here -->
+        <style>
+            .card {
+                border-radius: 5px;
+                height: 450px;
+            }
+
+            .card-text-body {
+                margin-top: 10px;
+                height: calc(100% - 300px);
+            }
+
+            .txt:hover {
+                text-decoration: underline;
+            }
+
+            p {
+                margin: 0;
+                padding: 2px;
+                text-align: justify;
+                text-justify: initial;
+                word-break: break-all;
+                line-height: 1;
+            }
+        </style>
+
+
+        <!-- Cards Start -->
+        <div class="row">
+            <?php
+            if ($result = $link->query($sql)) {
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_array()) { ?>
+                        <div class="col-sm-4">
+                            <div class="card mt-3">
+                                <div class="card-header text-center <?php echo ($row['sch_start_date'] > date("Y-m-d")) ? 'bg-success' : 'bg-danger'; ?>">
+                                    <i class="fa-solid fa-calendar-days px-2"></i><?php echo ($row['sch_start_date'] > date("Y-m-d")) ? "Launch Date: " . $row["sch_start_date"] : "Deadline: " . $row["sch_deadline"]; ?>
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title txt"><a class="text-decoration-none text-secondary" href="<?php echo $row['sch_link'] ?>" target="_blank" rel="noopener noreferrer"><?php echo $row["sch_name"] ?></a></h5>
+                                    <hr>
+                                    <div class="card-text-body">
+                                        <div class="card-text text-primary">Eligibility</div>
+                                        <ul>
+                                            <?php if (!empty($row['minority'])) { ?>
+                                                <li>
+                                                    <p><small class="text-muted"><?php echo $row['minority']; ?></small></p>
+                                                </li>
+                                            <?php } ?>
+                                            <?php if (!empty($row['sc_st'])) { ?>
+                                                <li>
+                                                    <p><small class="text-muted"><?php echo $row['sc_st']; ?></small></p>
+                                                </li>
+                                            <?php } ?>
+                                            <?php if (!empty($row['girls'])) { ?>
+                                                <li>
+                                                    <p><small class="text-muted"><?php echo $row['girls']; ?></small></p>
+                                                </li>
+                                                <?php } ?><?php if (!empty($row['community'])) { ?>
+                                                <li>
+                                                    <p><small class="text-muted"><?php echo $row['community']; ?></small></p>
+                                                </li>
+                                            <?php } ?>
+                                            <?php if (!empty($row['pwd'])) { ?>
+                                                <li>
+                                                    <p><small class="text-muted"><?php echo $row['pwd']; ?></small></p>
+                                                <?php } ?>
+                                                </li>
+                                                <?php if (!empty($row['athletic'])) { ?>
+                                                    <li>
+                                                        <p><small class="text-muted"><?php echo $row['athletic']; ?></small></p>
+                                                    </li>
+                                                <?php } ?>
+                                                <?php if (!empty($row['other_sch'])) { ?>
+                                                    <li>
+                                                        <p><small class="text-muted"><?php echo $row['other_sch']; ?></small></p>
+                                                    </li>
+                                                <?php } ?>
+                                                <ul>
+                                    </div>
+                                </div>
+                                <div class="card-footer d-flex justify-content-between">
+                                    <small> Last Updated On:</small>
+                                    <small class="text-muted"><?php echo $row['created_at'] ?></small>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="d-flex justify-content-center">
+                                        <div class="btn btn-sm"><i class="fas fa-eye" aria-hidden="true">
+                                                <?php echo "<a class='px-1 text-decoration-none text-muted' onclick=\"return confirm('Do you really want to Enable this scholarship?')\" href=\"../scholarship-operation/activate-scholarship.php?id=" . $row['id'] . " \">ENABLE</a>"; ?>
+                                                <script>
+                                                    document.getElementById('a.delete').on('click', function() {
+                                                        var choice = confirm('Enable this scholarship?');
+                                                        if (choice === true) {
+                                                            return true;
+                                                        }
+                                                        return false;
+                                                    });
+                                                </script>
+                                            </i></div>
+                                        <div class="btn btn-sm"><i class="fas fa-edit" aria-hidden="true">
+                                                <?php echo "<a class='px-1 text-decoration-none text-success' onclick=\"return confirm('Do you really want to modify this scholarship?')\" href=\"update-scholarship.php?id=" . $row['id'] . " \">MODIFY</a>"; ?>
+                                                <script>
+                                                    document.getElementById('a.delete').on('click', function() {
+                                                        var choice = confirm('Modify this scholarship?');
+                                                        if (choice === true) {
+                                                            return true;
+                                                        }
+                                                        return false;
+                                                    });
+                                                </script>
+                                            </i></div>
+                                        <div class="btn btn-sm"><i class="fa fa-trash" aria-hidden="true">
+                                                <?php echo "<a class='px-1 text-decoration-none text-danger' onclick=\"return confirm('Do you really want to delete this scholarship?')\" href=\"../scholarship-operation/delete-scholarship.php?id=" . $row['id'] . " \">DELETE</a>"; ?>
+                                                <script>
+                                                    document.getElementById('a.delete').on('click', function() {
+                                                        var choice = confirm('Delete this scholarship?');
+                                                        if (choice === true) {
+                                                            return true;
+                                                        }
+                                                        return false;
+                                                    });
+                                                </script>
+                                            </i></div>
+                                    </div>
+                                </div>
+                                <div class="card-footer text-center bg-primary">
+                                    <a href="../scholarship-operation/scholarship-details.php?id=<?php echo urlencode($row['id']) ?>" class="text-decoration-none text-white">View Scholarship</a>
+                                </div>
+                            </div>
+                        </div>
+            <?php    }
+                    // Free result set
+                    $result->free();
+                } else {
+                    echo "<br>";
+                    echo "There are currently no scholarships inactive.";
+                }
+            } else {
+                echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
+            }
+            ?>
+        </div>
+        <!-- Cards End -->
+
+
+
+        <!-- End of content  -->
+
+
+    </div>
+</div>
+</section>
--- Admin_Dashboard_Panel/notify.php
+++ Admin_Dashboard_Panel/notify.php
@@ -2,19 +2,54 @@
 include("admin-layout.php");
 ?>
 <title>SEND NOTIFICATION</title>
-        <div class="dash-content">
-            <div class="overview">
-                <div class="title">
-                    <i class="uil uil-tachometer-fast-alt"></i>
-                    <span class="text">SEND NOTIFICATION</span>
-                </div>
-    
-<!-- Add contents here -->
-
-<!-- End of content  -->
-
-
-            </div>
+<div class="dash-content">
+    <div class="overview">
+        <div class="title">
+            <i class="uil uil-tachometer-fast-alt"></i>
+            <span class="text">SEND NOTIFICATION</span>
         </div>
-    </section>
-        
+
+        <!-- Add contents here -->
+        <form action="" class="row g-3">
+            <div class="col-md-12">
+                <label for="receiver" class="form-label">To</label>
+                <select class="form-select" id="myInput" onclick="recName()" aria-label="Default select example" required>
+                    <option selected value="">Select...</option>
+                    <option value="all">All</option>
+                    <option value="personal" id="personal">Individual</option>
+                </select>
+            </div>
+            <div class="col-md-12 mt-3">
+                <input type="text" id="person-usn" name="person-usn" class="form-control" style="display:none" placeholder="Type USN">
+            </div>
+            <div class="col-md-12">
+                <label for="receiver" class="form-label">Subject</label>
+                <input type="text" class="form-control" id="receiver" placeholder="" required>
+            </div>
+            <div class="col-md-12">
+                <label for="body" class="form-label">Body</label>
+                <textarea class="form-control" id="body" rows="3" required></textarea>
+            </div>
+            <button type="submit" class="mx-2 btn btn-primary" style="width: 120px;">Send</button>
+        </form>
+        <!-- End of content  -->
+    </div>
+</div>
+</section>
+
+<script>
+    function recName() {
+        // Get the select element
+        var select = document.getElementById('myInput');
+        // Get the output text
+        var value = select.options[select.selectedIndex].value;
+        var text = document.getElementById("person-usn");
+
+        // If the checkbox is checked, display the output text
+        if (value === "personal") {
+            text.style.display = "block";
+        } else {
+            text.style.display = "none";
+        }
+    }
+</script>
